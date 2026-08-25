8ef68690601f9be66e78e9d1ce1231ea505f56f5
Kaito <kaitogamedev@gmail.com>
2026-08-25 18:37:25 -0300

    crate money

---
+++ source/money/money.gd
@@ -0,0 +1,12 @@
+class_name Money extends Node3D
+
+@onready var front_content: MoneyContent = $Front/ViewportMoney/FrontContent
+@onready var back_content: MoneyContent = $Back/ViewportMoney/BackContent
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ source/money/money_content.gd
@@ -0,0 +1,7 @@
+class_name MoneyContent extends Control
+
+@export var serial_number_orientation: Orientation = Orientation.HORIZONTAL
+
+@export var amount: int = 0
+@export var serial: String = ""
+@export var stamp: Texture2D
