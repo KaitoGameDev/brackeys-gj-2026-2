02096daf960bd36e7f131050be63672fa6aab113
Kaito <kaitogamedev@gmail.com>
2026-08-26 16:11:55 -0300

    create clients

---
+++ source/clients/client_resource.gd
@@ -0,0 +1,5 @@
+class_name ClientResource extends Resource
+
+@export var name: String = ""
+@export var texture: Texture2D
+@export var lines: Array[String] = []
---
+++ source/clients/client_spawner.gd
@@ -0,0 +1,4 @@
+class_name ClientSpawner extends Node3D
+
+var client_scene: PackedScene = preload("res://source/clients/client.tscn")
+var client_resources: Array[ClientResource]
