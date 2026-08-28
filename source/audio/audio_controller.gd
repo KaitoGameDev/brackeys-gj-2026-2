4802c514435cda74162f885e93c8e1e12cf9e7ee
Carlos Montes <carlosmontes002@gmail.com>
2026-08-27 22:57:12 -0300

    add audio controller (basic), create strikes UI

---
+++ source/audio/audio_controller.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+@export var bgm: Dictionary[String, AudioStream] = {}
+@export var sfx: Dictionary[String, AudioStream] = {}
+@export var interactions: Dictionary[String, AudioStream] = {}
---
+++ source/strikes/strikes_controller.gd
@@ -0,0 +1,23 @@
+class_name StrikesController extends Control
+
+var enabled_strike_texture: Texture2D = preload("res://assets/icons/enabled_x.png")
+var disabled_strike_texture: Texture2D = preload("res://assets/icons/disabled_x.png")
+
+var current_strikes: int = 0
+var strikes: Array[TextureRect] = []
+
+@onready var container: HBoxContainer = $HBoxContainer
+
+func _ready() -> void:
+	for child in container.get_children():
+		strikes.append(child)
+		
+	EventBus.on_event.connect(_on_event)
+	
+
+func _on_event(event: Object) -> void:
+	if event is MoneySwipedEvent:
+		# Hay que verificar porque no esta funcionando esta condicion		
+		if event.direction == MoneySwipedEvent.Direction.DOWN and event.money.money_resource.is_fake():
+			strikes[current_strikes].texture = enabled_strike_texture
+			current_strikes += 1
