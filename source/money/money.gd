b8666a15ce6163b05baf1bf6bbd9da56d9507093
Lascario Pacheco <lascariopacheco@hotmail.com>
2026-08-28 20:30:32 -0400

    fix: remove self_module from the Money instances

--- source/money/money.gd
+++ source/money/money.gd
@@ -16,11 +16,11 @@ func _ready() -> void:
 		front_content.add_serial(money_resource.front_serial)
 		front_content.bg_color.self_modulate = money_resource.front_color
 		front_content.stamp.texture = money_resource.front_stamp
-		front_content.stamp.self_modulate = money_resource.front_color
+		#front_content.stamp.self_modulate = money_resource.front_color
 		
 		back_content.top_left_value_label.text = str(money_resource.back_value)
 		back_content.bottom_right_value_label.text = str(money_resource.back_value)
 		back_content.bg_color.self_modulate = money_resource.back_color
 		back_content.add_serial(money_resource.back_serial)
 		back_content.stamp.texture = money_resource.back_stamp
-		back_content.stamp.self_modulate = money_resource.back_color
+		#back_content.stamp.self_modulate = money_resource.back_color
